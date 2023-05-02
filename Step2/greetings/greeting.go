0ba56f03ded2d3f8cfa024848e6d07fe80b73013
loveelf1@naver.com <dean@youfriend.kr>
2023-05-02 12:28:34 +0900

    Go practice Step3

--- Step2/greetings/greeting.go
+++ Step2/greetings/greeting.go
@@ -14,7 +14,8 @@ func Hello(name string) (string, error) {
 		return name, errors.New("empty name")
 	}
 	// Create a message using a random format.
-	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprintf(randomFormat(), name)
+	message := fmt.Sprintf(randomFormat())
 	return message, nil
 }
 
